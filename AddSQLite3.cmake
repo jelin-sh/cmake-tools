d7718dd121f93cc4eb2c97b62e04011720446e6e
朝霖 石 <skjsnb@163.com>
2023-02-17 10:38:51 +0800

    style: 🎨 update "AddSQLite3.cmake"

--- AddSQLite3.cmake
+++ AddSQLite3.cmake
@@ -3,10 +3,10 @@ include(FetchContent)
 message(STATUS "Download & Install SQLite3 ...")
 set(FETCHCONTENT_QUIET FALSE)
 fetchcontent_declare(
-    SQLite3
+    sqlite3
     GIT_REPOSITORY https://gitee.com/skjsnb/sqlite3-amalgamation.git
     GIT_PROGRESS TRUE
 )
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-MakeAvailable(SQLite3)
+MakeAvailable(sqlite3)
 message(STATUS "Download & Install SQLite3 ... Done")
