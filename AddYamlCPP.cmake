5272c77aa05f042c08cd3b1f8c1d14feb5d56a10
朝霖 石 <skjsnb@163.com>
2023-03-22 20:06:40 +0800

    feat: ✨ 取消YAML-CPP TAG控制

--- AddYamlCPP.cmake
+++ AddYamlCPP.cmake
@@ -5,7 +5,7 @@ message(STATUS "Initializing yaml-cpp ...")
 fetchcontent_declare(
     yaml
     GIT_REPOSITORY https://gitee.com/chohotech/yaml-cpp.git
-    GIT_TAG yaml-cpp-0.7.0
+    #GIT_TAG yaml-cpp-0.7.0
 )
 MakeAvailable(yaml)
 message(STATUS "Initializing yaml-cpp ... Done")
